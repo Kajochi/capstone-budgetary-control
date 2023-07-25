6b9473823a9683d59e5d7a0b79f7ab113b7c2eb3
christophgroneberg <christophgroneberg1@gmx.de>
2023-07-25 13:08:30 +0200

    add useState for entries, axios for get-request and useEffect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,35 @@
+import {Entry} from "./model/entry.ts";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import EntriesList from "./entriesList/EntriesList.tsx";
+
+
 
 
 export default function App() {
 
+  const [entries, setEntries] = useState<Entry[]>([])
+
+  function getAllEntries() {
+    axios.get('/api/entries').then((response) => {
+      setEntries(response.data)
+    })
+  }
+
+  useEffect(() => {
+    getAllEntries()
+  }, [])
 
   return (
     <>
+      <header>
+        <h1>Budgetary Control</h1>
+      </header>
+        <main>
+          <EntriesList entries={entries} />
+        </main>
+      </>
 
-    </>
   )
 }
 
