946a862f6713e332d5d7bea88155ccbf87509e15
christophgroneberg <christophgroneberg1@gmx.de>
2023-07-26 15:09:43 +0200

    add Routes to home and EntryAddUpdate.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,54 @@
-import {Entry} from "./model/Entry.ts";
-import {useEffect, useState} from "react";
-import axios from "axios";
-import EntriesList from "./entriesList/EntriesList.tsx";
-import './App.css';
+
 import AddIcon from '@mui/icons-material/Add';
-import {Button, IconButton} from "@mui/material";
+import {IconButton} from "@mui/material";
 import styled from "@emotion/styled";
+import {Route, Routes, Link} from "react-router-dom";
+import EntryAddUpdate from "./entryAddUpdate/EntryAddUpdate.tsx";
+import Home from "./home/Home.tsx";
 
 export default function App() {
 
-    const [entries, setEntries] = useState<Entry[]>([])
-
-    function getAllEntries() {
-        axios.get('/api/entries')
-            .then((response) => {
-                setEntries(response.data)
-            })
-    }
-
-    useEffect(() => {
-        getAllEntries()
-    }, [])
-
     return (
         <>
-            <header>
-                <h1 id={"mainTitle"}>Budgetary Control</h1>
-            </header>
-            <main>
-               <StyledIconButton  size={"small"}><AddIcon/></StyledIconButton>
-                <EntriesList entries={entries}/>
-            </main>
+            <StyledH1>Budgetary Control</StyledH1>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/add-entry"} element={<EntryAddUpdate/>}/>
+            </Routes>
+
+
+            <Link to={"/add-entry"}>
+                <StyledIconButton size={"small"}><AddIcon/></StyledIconButton>
+            </Link>
+            <SytledLink to={"/"}>
+                Home
+            </SytledLink>
+
+
         </>
 
     )
 }
 
-const StyledIconButton = styled(IconButton)`
-    background-color: #49e0e3;
-    &:hover {
-      background-color: #49e0e3;
-    transition: all 0.3s;
-    transform: scale(1.2);}
-    `;
+const StyledH1 = styled.h1`
 
+  font-family: "Roboto Light", sans-serif;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledIconButton = styled(IconButton)`
+  background-color: #49e0e3;
+
+  &:hover {
+    background-color: #49e0e3;
+    transition: all 0.3s;
+    transform: scale(1.2);
+  }
+`;
+
+const SytledLink = styled(Link)`
+  font-family: "Roboto Light", sans-serif;
+  display: flex;
+  justify-content: center;
+    `;
