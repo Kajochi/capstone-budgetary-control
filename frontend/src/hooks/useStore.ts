f7df840834f695596787bb9b73ee204ba2e1764e
christophgroneberg <christophgroneberg1@gmx.de>
2023-07-26 18:20:35 +0200

    add createEntry in useStore.ts

--- frontend/src/hooks/useStore.ts
+++ frontend/src/hooks/useStore.ts
@@ -1,19 +1,33 @@
 import {create} from "zustand";
 import axios from "axios";
 import {Entry} from "../model/Entry.ts";
+import {EntryWithNoId} from "../model/EntryWithNoId.ts";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
 type State = {
     entries: Entry[];
     getEntries: () => void;
+    createEntry: (requestBody: EntryWithNoId) => void;
 
 }
 
 
 export const useStore = create<State>((set) => ({
     entries: [],
+
     getEntries: () => {
-    axios.get("/api/entries")
-        .then((response) => {
-            set({entries: response.data});
-        })
-}}));
+        axios.get("/api/entries")
+            .then((response) => {
+                set({entries: response.data});
+            }).catch(error)
+    },
+
+    createEntry: (requestBody: EntryWithNoId) => {
+        axios.post("/api/entries", requestBody)
+            .catch(error)
+
+    }
+
+
+}));
