edf5eb8ecbe9c8e47c2ec3c9b7a9f292eec375de
christophgroneberg <christophgroneberg1@gmx.de>
2023-07-26 18:03:07 +0200

    add types Category and Interval in Entry.ts

--- frontend/src/model/Entry.ts
+++ frontend/src/model/Entry.ts
@@ -1,9 +1,12 @@
+import {Category} from "./Category.ts";
+import {Interval} from "./Interval.ts";
+
 export type Entry = {
     id: string;
     title: string;
     date: string;
     description: string;
     amount: string;
-    category: "INCOME" | "EXPENSE";
-    interval: "MONTHLY" | "HALF_YEARLY" | "QUARTERLY" | "YEARLY" | "ONCE";
+    category: Category;
+    interval: Interval;
 }
