befb106dcdd698d8829dbaf96710e7f75ef6872d
christophgroneberg <christophgroneberg1@gmx.de>
2023-08-02 09:45:49 +0200

    add FinanceReport package including classes and add FinanceReport Model plus Period enum

---
+++ backend/src/main/java/de/neuefische/capstone/backend/model/FinanceReport.java
@@ -0,0 +1,19 @@
+package de.neuefische.capstone.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class FinanceReport {
+
+    private final Period period;
+    private final BigDecimal totalIncome;
+    private final BigDecimal totalExpenses;
+    private final BigDecimal fixCosts;
+    private final BigDecimal variableCosts;
+    private final BigDecimal balance;
+
+}
---
+++ backend/src/main/java/de/neuefische/capstone/backend/model/Period.java
@@ -0,0 +1,8 @@
+package de.neuefische.capstone.backend.model;
+
+public enum Period {
+    MONTH,
+    QUARTER,
+    HALF_YEAR,
+    YEAR
+}
